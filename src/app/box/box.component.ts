cba3cf6aef2bfe4b3712f6e5182fbc76471ed58b
Ayush Kumar <ayushkumar2303@gmail.com>
2021-04-25 17:52:25 +0530

    Move individual boxes with arrow keys or WASD

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -12,7 +12,7 @@ export class BoxComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  // geenrate box id as unique timestamp
   boxId: any = Date.now();
   @Input() zindex: number = 1;
   @Input() mtop = 0 ;
@@ -20,9 +20,48 @@ export class BoxComponent implements OnInit {
   @Output() output = new EventEmitter();
 
   logDetails(event){
-    console.log(event);
+    // console.lsog(event);
     // console.log(event.target.id);
+  }
+
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    console.log(event);
+    console.log((event.target as HTMLElement).id);
+    this.moveBox(event.key, (event.target as HTMLElement).id, event);
+  }
+
+  moveBox(key, id, event){
+    // console.log(key , "pressed with id"+id );
+    // console.log(event.target.id);
+    var box = document.getElementById(id);
+    console.log(box);
+    switch(key){
+      case "w":;
+      case "W":;
+      case "ArrowUp": this.mtop= (this.mtop-1)>0?this.mtop-1:this.mtop; 
+                      box.style.marginTop = this.mtop.toString();
+                      break;
+      case "s":;
+      case "S":;
+      case "ArrowDown": this.mtop= (this.mtop+1)<93?this.mtop+1:this.mtop; 
+                      box.style.marginTop = this.mtop.toString();
+                      break;
+      case "a":;
+      case "A":;
+      case "ArrowLeft": this.mleft= (this.mleft-1)>0?this.mleft-1:this.mleft; 
+                      box.style.marginLeft = this.mtop.toString();
+                      break;
+      case "d":;
+      case "D":;
+      case "ArrowRight": this.mleft= (this.mleft+1)<77?this.mleft+1:this.mleft;
+                      box.style.marginLeft = this.mtop.toString(); 
+                      break;
+      default: console.log(key +" pressed. did nothing");
+    }
     
+
+
+
   }
 
 
